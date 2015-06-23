8fa423535a9a999e2ea607d2cc2f216cc0bc1279
Lennart Hildebrandt <hello@lennerd.com>
2015-06-23 16:20:50 +0200

    Revert fix for Firefox to fix performance on chrome.

--- app/client/stores/vis.js
+++ app/client/stores/vis.js
@@ -55,7 +55,7 @@ module.exports = Reflux.createStore({
 
     VisActions.update(this.scale(map.getZoom()), this.positionMapper);
 
-    this.triggerAsync(this.state);
+    this.trigger(this.state);
   },
 
   onMoveEnd: function(map) {
@@ -66,7 +66,7 @@ module.exports = Reflux.createStore({
       transform: Immutable.Map({ translate: immutablePoint(bounds.min) })
     });
 
-    this.triggerAsync(this.state);
+    this.trigger(this.state);
   },
 
   onZoomAnim: function(map, event) {
@@ -81,7 +81,7 @@ module.exports = Reflux.createStore({
       })
     });
 
-    this.triggerAsync(this.state);
+    this.trigger(this.state);
   },
 
   getInitialState: function() {
