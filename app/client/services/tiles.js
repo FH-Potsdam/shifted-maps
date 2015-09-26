175d16d38fd397a5eed06df492e521528b19b007
Lennart Hildebrandt <hello@lennerd.com>
2015-09-26 18:56:30 +0200

    Use setTimeout to not block I/O.

--- app/client/services/tiles.js
+++ app/client/services/tiles.js
@@ -6,12 +6,12 @@ const MAP_URL_SUFFIX = (L.Browser.retina ? '@2x' : '')  + '.png?access_token=' +
 export function createTileRequest(place, map, radiusScale, radiusRangeScale, done) {
   let { location, radius, duration} = place;
 
-  computeZoom(location, radius, map, function(error, zoom) {
-    computeSize(duration, map, zoom, radiusScale, radiusRangeScale, function(error, size) {
-      let url = computeTileUrl(location, zoom, size);
+  setTimeout(function() {
+    let zoom = computeZoom(location, radius, map),
+      size = computeSize(duration, map, zoom, radiusScale, radiusRangeScale),
+      url = computeTileUrl(location, zoom, size);
 
-      done(null, new Request(place.id, url));
-    })
+    done(null, new Request(place.id, url));
   });
 }
 
@@ -37,62 +37,50 @@ class Request {
   }
 }
 
-function computeZoom(location, radius, map, done) {
-  toBounds(location, place_to_bounds_meters, function(error, bounds) {
-    getBoundsZoom(map, bounds, radius * 2, function(error, zoom) {
-      done(null, zoom);
-    });
-  });
+function computeZoom(location, radius, map) {
+  let bounds = toBounds(location, place_to_bounds_meters);
+
+  return getBoundsZoom(map, bounds, radius * 2);
 }
 
-function computeSize(duration, map, zoom, radiusScale, radiusRangeScale, done) {
-  process.nextTick(function() {
-    let scale = zoom / map.getMaxZoom();
+function computeSize(duration, map, zoom, radiusScale, radiusRangeScale) {
+  let scale = zoom / map.getMaxZoom();
 
-    radiusScale = radiusScale.copy()
-      .range(radiusRangeScale(scale));
+  radiusScale = radiusScale.copy()
+    .range(radiusRangeScale(scale));
 
-    let size = Math.max(place_min_tile_size, Math.ceil(radiusScale(duration) * 2));
-
-    done(null, size);
-  });
+  return Math.max(place_min_tile_size, Math.ceil(radiusScale(duration) * 2));
 }
 
 // @TODO Replace with leaflet function, if mapbox js library was updated. (v1.0)
-function toBounds(location, sizeInMeters, done) {
-  process.nextTick(function() {
-    var latAccuracy = 180 * sizeInMeters / 40075017,
-      lngAccuracy = latAccuracy / Math.cos((Math.PI / 180) * location.lat);
+function toBounds(location, sizeInMeters) {
+  var latAccuracy = 180 * sizeInMeters / 40075017,
+    lngAccuracy = latAccuracy / Math.cos((Math.PI / 180) * location.lat);
 
-    let bounds = L.latLngBounds(
-      [location.lat - latAccuracy, location.lng - lngAccuracy],
-      [location.lat + latAccuracy, location.lng + lngAccuracy]);
-
-    done(null, bounds);
-  });
+  return L.latLngBounds(
+    [location.lat - latAccuracy, location.lng - lngAccuracy],
+    [location.lat + latAccuracy, location.lng + lngAccuracy]);
 }
 
 // Taken from Leaflet libraries Map object but added own size parameter.
-function getBoundsZoom(map, bounds, size, done) {
-  process.nextTick(function() {
-    bounds = L.latLngBounds(bounds);
-    size = L.point([size, size]);
+function getBoundsZoom(map, bounds, size) {
+  bounds = L.latLngBounds(bounds);
+  size = L.point([size, size]);
 
-    var zoom = map.getMinZoom(),
-      maxZoom = map.getMaxZoom(),
-      nw = bounds.getNorthWest(),
-      se = bounds.getSouthEast(),
-      zoomNotFound = true,
-      boundsSize;
+  var zoom = map.getMinZoom(),
+    maxZoom = map.getMaxZoom(),
+    nw = bounds.getNorthWest(),
+    se = bounds.getSouthEast(),
+    zoomNotFound = true,
+    boundsSize;
 
-    do {
-      zoom++;
-      boundsSize = map.project(se, zoom).subtract(map.project(nw, zoom)).floor();
-      zoomNotFound = size.contains(boundsSize);
-    } while (zoomNotFound && zoom <= maxZoom);
+  do {
+    zoom++;
+    boundsSize = map.project(se, zoom).subtract(map.project(nw, zoom)).floor();
+    zoomNotFound = size.contains(boundsSize);
+  } while (zoomNotFound && zoom <= maxZoom);
 
-    done(null, zoom);
-  });
+  return zoom - 1;
 }
 
 function computeTileUrl(location, zoom, size) {
