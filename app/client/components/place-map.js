aec85e585c7cb952a10590229e7ac90d25138cc2
Lennart Hildebrandt <hello@lennerd.com>
2015-11-10 14:04:03 +0100

    Smooth map image loading.

--- app/client/components/place-map.js
+++ app/client/components/place-map.js
@@ -1,33 +1,95 @@
 import React, { Component } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import PlaceClip from './place-clip';
 
+const EMPTY_IMAGE_URL = 'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=';
+
 class PlaceMap extends Component {
-  shouldComponentUpdate(nextProps) {
+  constructor(props) {
+    super(props);
+
+    this.image = null;
+
+    this.state = {
+      image: null
+    };
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
     let { node } = this.props,
       nextNode = nextProps.node;
 
-    return node.radius !== nextNode.radius || node.tileURL !== nextNode.tileURL;
+    return node.radius !== nextNode.radius ||
+      node.tileURL !== nextNode.tileURL ||
+      this.state.image !== nextState.image;
   }
 
-  componentWillUpdate(nextProps) {
-    if (nextProps.node.tileURL != null)
+  componentWillMount() {
+    this.updateImage(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.updateImage(nextProps);
+  }
+
+  updateImage(nextProps) {
+    let { tileURL } = nextProps.node;
+
+    if (tileURL == null || this.state.url === tileURL)
       return;
 
-    this.oldTileURL = this.props.node.tileURL || this.oldTile;
+    this.abortImage();
+
+    let image = this.image = new Image();
+
+    image.onload = () => {
+      this.image = null;
+
+      this.setState({ image });
+    };
+
+    image.src = tileURL;
+  }
+
+  abortImage() {
+    let image = this.image;
+
+    if (image == null)
+      return;
+
+    image.onload = function() { return false };
+    image.src = EMPTY_IMAGE_URL;
+
+    this.image = null;
+  }
+
+  renderImage() {
+    let { image } = this.state;
+
+    if (image == null)
+      return null;
+
+    let src = image.src,
+      size = image.width,
+      radius = size / 2;
+
+    return <image key={src} x={-radius} y={-radius} width={size} height={size} xlinkHref={src} />;
   }
 
   render() {
     let { node } = this.props,
       { radius } = node,
-      tileURL = node.tileURL || this.oldTileURL,
       clipPath = 'url(#' + PlaceClip.createId(node) + ')',
-      size = radius * 2;
+      size = radius * 2,
+      image = this.renderImage();
 
     return (
-      <g className="place-map"
-         clipPath={clipPath}>
+      <g className="place-map" clipPath={clipPath}>
         <rect className="place-map-background" x={-radius} y={-radius} width={size} height={size} />
-        <image x={-radius} y={-radius} width={size} height={size} xlinkHref={tileURL} />
+        <ReactCSSTransitionGroup transitionName="fade" component="g"
+                                 transitionEnterTimeout={400} transitionLeaveTimeout={400}>
+          {image}
+        </ReactCSSTransitionGroup>
         <circle className="place-map-dot" r="3" cx="0" cy="0" />
       </g>
     );
