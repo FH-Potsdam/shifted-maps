83b5f6e63fa6a0c771f1c0c21ee0011ed9173b6a
Lennart Hildebrandt <hello@lennerd.com>
2018-10-25 12:22:16 +0200

    Fix place stroke width on mobile.

--- stores/config.ts
+++ stores/config.ts
@@ -37,7 +37,7 @@ const PLACE_STROKE_WIDTH_RANGE_MIN_SCALE = scaleLinear<[number, number]>()
 const PLACE_STROKE_WIDTH_RANGE_MAX_SCALE = scaleLinear<[number, number]>()
   .domain(SCREEN_WIDTH_DOMAIN)
   .clamp(true)
-  .range([/* min width */ [1, 3], /* max width */ [5, 15]]);
+  .range([/* min width */ [4, 8], /* max width */ [5, 15]]);
 
 export function createPlaceStrokeWidthRangeScale(width: number) {
   const rangeMin = PLACE_STROKE_WIDTH_RANGE_MIN_SCALE(width);
